e7563df8c8825274e6396a28f4adc53df8389ee1
kayyum-5b803176 <kayyum954f@gmail.com>
2026-08-16 12:31:04 +0530

    feat-timer-card-match-task-card-width

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,7 +34,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 1
-        versionName = "4.7.12"
+        versionName = "4.7.13"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     signingConfigs {
