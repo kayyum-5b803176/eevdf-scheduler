c92f63b85a167ed5c90131bad6e3bce057a84969
kayyum-5b803176 <kayyum954f@gmail.com>
2026-08-17 12:49:03 +0530

    fix call slot menu

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,7 +34,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 1
-        versionName = "4.8.1"
+        versionName = "4.8.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     signingConfigs {
