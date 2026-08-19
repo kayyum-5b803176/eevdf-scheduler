4646b5da3487e5210fa66f8ac29c10f38ef3a457
kayyum-5b803176 <kayyum954f@gmail.com>
2026-08-19 19:51:27 +0530

    improvement to missing pages, dark mode

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,7 +34,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 1
-        versionName = "4.10.2"
+        versionName = "4.10.3"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     signingConfigs {
