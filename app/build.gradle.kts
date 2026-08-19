7aaa6fa7c0e33992ec84e56e461e028845ae77fa
kayyum-5b803176 <kayyum954f@gmail.com>
2026-08-19 20:09:52 +0530

    settings-ui-unify-v4.10.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,7 +34,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 1
-        versionName = "4.10.3"
+        versionName = "4.10.4"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     signingConfigs {
