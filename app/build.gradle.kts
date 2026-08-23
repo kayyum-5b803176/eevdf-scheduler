0656e9996c28660ec9e706f51aa18fffc510f572
kayyum-5b803176 <kayyum954f@gmail.com>
2026-08-23 21:50:21 +0530

    stats-shadow-fix-4.20.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,7 +34,7 @@ android {
         minSdk = 31
         targetSdk = 34
         versionCode = 1
-        versionName = "4.20.5"
+        versionName = "4.20.6"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     signingConfigs {
