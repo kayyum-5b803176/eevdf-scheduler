9024bb16b12bbefa08ce536c98cc1bee2d97e88f
kayyum-5b803176 <kayyum954f@gmail.com>
2026-08-24 00:21:56 +0530

    unified-ui-tokens phase 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,7 +34,7 @@ android {
         minSdk = 31
         targetSdk = 34
         versionCode = 1
-        versionName = "4.21.1"
+        versionName = "4.21.5"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     signingConfigs {
