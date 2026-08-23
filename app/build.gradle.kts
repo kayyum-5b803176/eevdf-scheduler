3ea6925baf0780f9b95f50a5adfd7829581a37fb
kayyum-5b803176 <kayyum954f@gmail.com>
2026-08-23 21:45:52 +0530

    edit-task-shadow-fix-4.20.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,7 +34,7 @@ android {
         minSdk = 31
         targetSdk = 34
         versionCode = 1
-        versionName = "4.20.4"
+        versionName = "4.20.5"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     signingConfigs {
