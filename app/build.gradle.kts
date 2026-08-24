3a5a7335b476128f1670d4a676dd1b26ffa0b798
kayyum-5b803176 <kayyum954f@gmail.com>
2026-08-24 16:03:10 +0530

    stats-calendar-tokenised-4.24.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,7 +34,7 @@ android {
         minSdk = 31
         targetSdk = 34
         versionCode = 1
-        versionName = "4.24.3"
+        versionName = "4.24.4"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     signingConfigs {
