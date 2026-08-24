ffaa2231fff82c6624dcba5e98131cf9549715be
kayyum-5b803176 <kayyum954f@gmail.com>
2026-08-24 12:38:56 +0530

    edit-task-tokenised-4.23.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,7 +34,7 @@ android {
         minSdk = 31
         targetSdk = 34
         versionCode = 1
-        versionName = "4.23.0"
+        versionName = "4.23.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     signingConfigs {
