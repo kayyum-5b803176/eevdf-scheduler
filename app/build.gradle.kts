56e8db21a8b9f85c60425b255443162328042861
kayyum-5b803176 <kayyum954f@gmail.com>
2026-08-24 18:10:00 +0530

    palette-93-token-4.27.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,7 +34,7 @@ android {
         minSdk = 31
         targetSdk = 34
         versionCode = 1
-        versionName = "4.26.0"
+        versionName = "4.27.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     signingConfigs {
