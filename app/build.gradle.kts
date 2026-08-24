08836eb5d600014f0ce8fcd28545674a14b11898
kayyum-5b803176 <kayyum954f@gmail.com>
2026-08-24 18:44:30 +0530

    settings-divider-fix-4.27.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,7 +34,7 @@ android {
         minSdk = 31
         targetSdk = 34
         versionCode = 1
-        versionName = "4.27.1"
+        versionName = "4.27.3"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     signingConfigs {
