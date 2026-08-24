71676a18d1136706f39d8647bfa8b11cd1040628
kayyum-5b803176 <kayyum954f@gmail.com>
2026-08-24 17:37:02 +0530

    ui-material-palette-4.26.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,7 +34,7 @@ android {
         minSdk = 31
         targetSdk = 34
         versionCode = 1
-        versionName = "4.25.2"
+        versionName = "4.26.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     signingConfigs {
