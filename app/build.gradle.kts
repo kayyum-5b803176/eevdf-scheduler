cfce1cbc3966d93d9616d4a20b9cad4fb06d8bbb
kayyum-5b803176 <kayyum954f@gmail.com>
2026-08-24 18:16:19 +0530

    fix-ui-issues-4.27.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,7 +34,7 @@ android {
         minSdk = 31
         targetSdk = 34
         versionCode = 1
-        versionName = "4.27.0"
+        versionName = "4.27.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     signingConfigs {
