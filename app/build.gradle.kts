b6955c8c9155f761f1771352257c1d2ea258ef9a
kayyum-5b803176 <kayyum954f@gmail.com>
2026-08-24 15:57:40 +0530

    stats-overview-tokenised-4.24.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,7 +34,7 @@ android {
         minSdk = 31
         targetSdk = 34
         versionCode = 1
-        versionName = "4.24.2"
+        versionName = "4.24.3"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     signingConfigs {
