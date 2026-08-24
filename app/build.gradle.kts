1a7b30673f5a0b5add8324676272529f043394dc
kayyum-5b803176 <kayyum954f@gmail.com>
2026-08-24 17:18:15 +0530

    color-material-palette-4.25.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,7 +34,7 @@ android {
         minSdk = 31
         targetSdk = 34
         versionCode = 1
-        versionName = "4.25.0"
+        versionName = "4.25.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     signingConfigs {
