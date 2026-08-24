0ffe6999ce072e0402655e15936fb38c0124d57d
kayyum-5b803176 <kayyum954f@gmail.com>
2026-08-24 16:05:50 +0530

    stats-all-clean-4.24.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,7 +34,7 @@ android {
         minSdk = 31
         targetSdk = 34
         versionCode = 1
-        versionName = "4.24.4"
+        versionName = "4.24.5"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     signingConfigs {
