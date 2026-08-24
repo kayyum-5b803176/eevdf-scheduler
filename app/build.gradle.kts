fe4da0206471d6059ce68f724f91d0faaeb9b820
kayyum-5b803176 <kayyum954f@gmail.com>
2026-08-24 20:47:38 +0530

    fully-tokenised-4.28.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,7 +34,7 @@ android {
         minSdk = 31
         targetSdk = 34
         versionCode = 1
-        versionName = "4.27.3"
+        versionName = "4.28.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     signingConfigs {
