24b50d85a8ebb2fcdd427cb06f45eab5c4a78409
kayyum-5b803176 <kayyum954f@gmail.com>
2026-08-25 12:32:00 +0530

    styles-applied-5.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,7 +34,7 @@ android {
         minSdk = 31
         targetSdk = 34
         versionCode = 1
-        versionName = "5.0.3"
+        versionName = "5.1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     signingConfigs {
