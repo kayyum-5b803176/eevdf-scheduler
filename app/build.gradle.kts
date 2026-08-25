50f52dcd7aa13770d893dbcee607a0693c061593
kayyum-5b803176 <kayyum954f@gmail.com>
2026-08-25 13:30:13 +0530

    fix-settings-crash_fix-main-card-margins

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,7 +34,7 @@ android {
         minSdk = 31
         targetSdk = 34
         versionCode = 1
-        versionName = "5.1.0"
+        versionName = "5.1.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     signingConfigs {
